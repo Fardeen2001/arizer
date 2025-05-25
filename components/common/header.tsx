5728b260e4e6d65e10aaf3f73969aecc4ab070ec
Fardeen2001 <1si20et403@sit.ac.in>
2025-05-25 23:15:01 +0530

    header done

---
+++ components/common/header.tsx
@@ -0,0 +1,50 @@
+import { FileText } from "lucide-react";
+import React from "react";
+import { Button } from "../ui/button";
+import NavLink from "./nav-link";
+
+const Header = () => {
+  const isLoggedIn = false;
+  return (
+    <nav
+      className={
+        "container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto"
+      }
+    >
+      <div className="flex lg:flex-1">
+        <NavLink
+          href={"/"}
+          className="flex items-center gap-1 lg:gap-2 shrink-0"
+        >
+          <FileText
+            className={
+              "w-5 h-8 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out"
+            }
+          />
+          <span className="text-gray-900 lg:text-xl font-extrabold">
+            Arizer Ai
+          </span>
+        </NavLink>
+      </div>
+      <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
+        <NavLink href={"/#pricing"}>Pricing</NavLink>
+        {isLoggedIn && <NavLink href={"/dashboard"}>Your Summaries</NavLink>}
+      </div>
+      <div className="flex lg:justify-end lg:flex-1">
+        {isLoggedIn ? (
+          <div className="flex items-center gap-2">
+            <NavLink href={"/upload"}>Upload A PDF</NavLink>
+            <div>Pro</div>
+            <Button>User</Button>
+          </div>
+        ) : (
+          <div>
+            <NavLink href={"/sign-in"}>Sign In</NavLink>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
